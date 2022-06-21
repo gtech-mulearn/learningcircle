4f9cc77e131189270da3f15bf4ba03d9385a1c32
Aswin Asok <aswinasokofficial@gmail.com>
2022-06-21 22:23:39 +0530

    Got District, College and Interests

--- src/App.js
+++ src/App.js
@@ -10,36 +10,66 @@ function App() {
   const [colleges, setColleges] = useState("");
 
   const [district, setDistrict] = useState("");
-  const [colelge, setCollege] = useState("");
+  const [college, setCollege] = useState("");
 
   const [interests, setInterests] = useState("");
   const [interest, setInterest] = useState("");
 
   useEffect(() => {
-    axios
-      .get(`${process.env.REACT_APP_BACKEND_URL}/districts`)
-      .then(function (response) {
-        setDistricts(response.data.data);
-      })
-      .catch(function (error) {
-        console.log(error);
-      });
+    if (!districts) {
+      axios
+        .get(`${process.env.REACT_APP_BACKEND_URL}/districts`)
+        .then(function (response) {
+          setDistricts(response.data.data);
+        })
+        .catch(function (error) {
+          console.log(error);
+        });
+    }
 
-    axios
-      .get(`${process.env.REACT_APP_BACKEND_URL}/interests`)
-      .then(function (response) {
-        setInterests(response.data.data);
-      })
-      .catch(function (error) {
-        console.log(error);
-      });
-  }, []);
+    if (!interests) {
+      axios
+        .get(`${process.env.REACT_APP_BACKEND_URL}/interests`)
+        .then(function (response) {
+          setInterests(response.data.data);
+        })
+        .catch(function (error) {
+          console.log(error);
+        });
+    }
+
+    if (district) {
+      axios
+        .get(`${process.env.REACT_APP_BACKEND_URL}/colleges/${district}`)
+        .then(function (response) {
+          setColleges(response.data.data);
+        })
+        .catch(function (error) {
+          console.log(error);
+        });
+    }
+  }, [district, college]);
 
   return (
     <div className="App">
       <Router>
         <Routes>
-          <Route path="/" element={<Home />} />
+          <Route
+            path="/"
+            element={
+              <Home
+                districts={districts}
+                district={district}
+                setDistrict={setDistrict}
+                colleges={colleges}
+                setCollege={setCollege}
+                college={college}
+                interests={interests}
+                setInterest={setInterest}
+                interest={interest}
+              />
+            }
+          />
         </Routes>
       </Router>
     </div>
--- src/Pages/LearningCircle/Home/Home.jsx
+++ src/Pages/LearningCircle/Home/Home.jsx
@@ -2,13 +2,98 @@ import React from "react";
 import Navbar from "../../../Components/Navbar/Navbar";
 import Footer from "../../../Components/Footer/Footer";
 
-const Home = () => {
-  return (
+import Box from "@mui/material/Box";
+import InputLabel from "@mui/material/InputLabel";
+import MenuItem from "@mui/material/MenuItem";
+import FormControl from "@mui/material/FormControl";
+import Select from "@mui/material/Select";
+
+const Home = ({
+  districts,
+  setDistrict,
+  colleges,
+  setCollege,
+  district,
+  college,
+  interests,
+  setInterest,
+  interest,
+}) => {
+  if (districts) {
+    return (
+      <>
+        <Navbar />
+
+        <Box sx={{ minWidth: 120 }}>
+          <FormControl fullWidth>
+            <InputLabel id="demo-simple-select-label">
+              Select District
+            </InputLabel>
+            <Select
+              labelId="demo-simple-select-label"
+              id="demo-simple-select"
+              label="Select District"
+              value={district}
+              onChange={(e) => setDistrict(e.target.value)}
+            >
+              {districts.map((district) => (
+                <MenuItem value={district}>{district}</MenuItem>
+              ))}
+            </Select>
+          </FormControl>
+        </Box>
+
+        {colleges && (
+          <Box sx={{ minWidth: 120 }}>
+            <FormControl fullWidth>
+              <InputLabel id="demo-simple-select-label">
+                Select College
+              </InputLabel>
+              <Select
+                labelId="demo-simple-select-label"
+                id="demo-simple-select"
+                label="Select College"
+                value={college}
+                onChange={(e) => setCollege(e.target.value)}
+              >
+                {colleges.map((college) => (
+                  <MenuItem value={college.code}>{college.name}</MenuItem>
+                ))}
+              </Select>
+            </FormControl>
+          </Box>
+        )}
+
+        {interests && college && (
+          <Box sx={{ minWidth: 120 }}>
+            <FormControl fullWidth>
+              <InputLabel id="demo-simple-select-label">
+                Select Interest Group
+              </InputLabel>
+              <Select
+                labelId="demo-simple-select-label"
+                id="demo-simple-select"
+                label="Select College"
+                value={interest}
+                onChange={(e) => setInterest(e.target.value)}
+              >
+                {interests.map((interest) => (
+                  <MenuItem value={interest.id}>{interest.title}</MenuItem>
+                ))}
+              </Select>
+            </FormControl>
+          </Box>
+        )}
+        <Footer />
+      </>
+    );
+  } else {
     <>
       <Navbar />
+      <p>Loading....</p>
       <Footer />
-    </>
-  );
+    </>;
+  }
 };
 
 export default Home;
