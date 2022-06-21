d6fca3a2afa102c667ac876c325dfae841fe7544
Aswin Asok <aswinasokofficial@gmail.com>
2022-06-21 21:22:03 +0530

    District and Interest Retrived

--- src/App.js
+++ src/App.js
@@ -3,8 +3,38 @@ import "./App.css";
 import React, { useEffect, useState } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./Pages/LearningCircle/Home/Home";
+import axios from "axios";
 
 function App() {
+  const [districts, setDistricts] = useState("");
+  const [colleges, setColleges] = useState("");
+
+  const [district, setDistrict] = useState("");
+  const [colelge, setCollege] = useState("");
+
+  const [interests, setInterests] = useState("");
+  const [interest, setInterest] = useState("");
+
+  useEffect(() => {
+    axios
+      .get(`${process.env.REACT_APP_BACKEND_URL}/districts`)
+      .then(function (response) {
+        setDistricts(response.data.data);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+
+    axios
+      .get(`${process.env.REACT_APP_BACKEND_URL}/interests`)
+      .then(function (response) {
+        setInterests(response.data.data);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  }, []);
+
   return (
     <div className="App">
       <Router>
