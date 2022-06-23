26752ec770d8ab6f34782e54c49ca9b6732723fe
Aswin Asok <aswinasokofficial@gmail.com>
2022-06-23 22:56:36 +0530

    Styling Home Page

--- src/Pages/LearningCircle/Home/Home.jsx
+++ src/Pages/LearningCircle/Home/Home.jsx
@@ -2,6 +2,9 @@ import React, { useEffect, useState } from "react";
 import Navbar from "../../../Components/Navbar/Navbar";
 import Footer from "../../../Components/Footer/Footer";
 
+import styles from "./Home.module.css";
+import learningcircles from "./assets/learningcircles.jpg";
+
 import Box from "@mui/material/Box";
 import InputLabel from "@mui/material/InputLabel";
 import MenuItem from "@mui/material/MenuItem";
@@ -13,6 +16,7 @@ import CardContent from "@mui/material/CardContent";
 import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
 import axios from "axios";
+import { margin } from "@mui/system";
 
 const Home = ({
   districts,
@@ -47,91 +51,98 @@ const Home = ({
       <>
         <Navbar />
 
-        <Box sx={{ minWidth: 120 }}>
-          <FormControl fullWidth>
-            <InputLabel id="demo-simple-select-label">
-              Select District
-            </InputLabel>
-            <Select
-              labelId="demo-simple-select-label"
-              id="demo-simple-select"
-              label="Select District"
-              value={district}
-              onChange={(e) => setDistrict(e.target.value)}
-            >
-              {districts.map((district) => (
-                <MenuItem value={district}>{district}</MenuItem>
-              ))}
-            </Select>
-          </FormControl>
-        </Box>
-
-        {colleges && (
-          <Box sx={{ minWidth: 120 }}>
-            <FormControl fullWidth>
-              <InputLabel id="demo-simple-select-label">
-                Select College
-              </InputLabel>
-              <Select
-                labelId="demo-simple-select-label"
-                id="demo-simple-select"
-                label="Select College"
-                value={college}
-                onChange={(e) => setCollege(e.target.value)}
-              >
-                {colleges.map((college) => (
-                  <MenuItem value={college.code}>{college.name}</MenuItem>
-                ))}
-              </Select>
-            </FormControl>
-          </Box>
-        )}
-
-        {interests && college && (
-          <Box sx={{ minWidth: 120 }}>
-            <FormControl fullWidth>
-              <InputLabel id="demo-simple-select-label">
-                Select Interest Group
-              </InputLabel>
-              <Select
-                labelId="demo-simple-select-label"
-                id="demo-simple-select"
-                label="Select College"
-                value={interest}
-                onChange={(e) => setInterest(e.target.value)}
-              >
-                {interests.map((interest) => (
-                  <MenuItem value={interest.id}>{interest.title}</MenuItem>
-                ))}
-              </Select>
-            </FormControl>
-          </Box>
-        )}
-
-        {teams.length > 0 &&
-          teams.map((team) => (
-            <Card sx={{ minWidth: 275 }}>
-              <CardContent>
-                <Typography
-                  sx={{ fontSize: 14 }}
-                  color="text.secondary"
-                  gutterBottom
+        <div className={styles.body_container}>
+          <img src={learningcircles} alt="" className={styles.mimage} />
+          <div className={styles.form}>
+            <Box sx={{ minWidth: 300, maxWidth: 300, margin: 1.5 }}>
+              <FormControl fullWidth>
+                <InputLabel id="demo-simple-select-label">
+                  Select District
+                </InputLabel>
+                <Select
+                  labelId="demo-simple-select-label"
+                  id="demo-simple-select"
+                  label="Select District"
+                  value={district}
+                  onChange={(e) => setDistrict(e.target.value)}
                 >
-                  {interest}
-                </Typography>
-                <Typography variant="h5" component="div">
-                  {team.lead}
-                </Typography>
-                <Typography sx={{ mb: 1.5 }} color="text.secondary">
-                  {team.code}
-                </Typography>
-                <Typography variant="body2">{team.count}</Typography>
-              </CardContent>
-              <CardActions>
-                <Button size="small">Learn More</Button>
-              </CardActions>
-            </Card>
-          ))}
+                  {districts.map((district) => (
+                    <MenuItem value={district}>{district}</MenuItem>
+                  ))}
+                </Select>
+              </FormControl>
+            </Box>
+
+            {colleges && (
+              <Box sx={{ minWidth: 300, maxWidth: 300, margin: 1.5 }}>
+                <FormControl fullWidth>
+                  <InputLabel id="demo-simple-select-label">
+                    Select College
+                  </InputLabel>
+                  <Select
+                    labelId="demo-simple-select-label"
+                    id="demo-simple-select"
+                    label="Select College"
+                    value={college}
+                    onChange={(e) => setCollege(e.target.value)}
+                  >
+                    {colleges.map((college) => (
+                      <MenuItem value={college.code}>{college.name}</MenuItem>
+                    ))}
+                  </Select>
+                </FormControl>
+              </Box>
+            )}
+
+            {interests && college && (
+              <Box sx={{ minWidth: 300, margin: 1.5 }}>
+                <FormControl fullWidth>
+                  <InputLabel id="demo-simple-select-label">
+                    Select Interest Group
+                  </InputLabel>
+                  <Select
+                    labelId="demo-simple-select-label"
+                    id="demo-simple-select"
+                    label="Select College"
+                    value={interest}
+                    onChange={(e) => setInterest(e.target.value)}
+                  >
+                    {interests.map((interest) => (
+                      <MenuItem value={interest.id}>{interest.title}</MenuItem>
+                    ))}
+                  </Select>
+                </FormControl>
+              </Box>
+            )}
+
+            <div className={styles.circles}>
+              {teams.length > 0 &&
+                teams.map((team) => (
+                  <Card sx={{ minWidth: 275, margin: 3 }}>
+                    <CardContent>
+                      <Typography
+                        sx={{ fontSize: 14 }}
+                        color="text.secondary"
+                        gutterBottom
+                      >
+                        Interest Group: {interest}
+                      </Typography>
+                      <Typography variant="h5" component="div">
+                        Lead: {team.lead}
+                      </Typography>
+                      <Typography sx={{ mb: 1.5 }} color="text.secondary">
+                        Team Code: {team.code}
+                      </Typography>
+                      <Typography variant="body2">Member Count: {team.count}</Typography>
+                    </CardContent>
+                    <CardActions>
+                      <Button size="small">Join Group Now!</Button>
+                    </CardActions>
+                  </Card>
+                ))}
+            </div>
+          </div>
+        </div>
 
         <Footer />
       </>
