5bb3df9cc2618e96666f9784dd4a6e2ca415b37b
Aswin Asok <aswinasokofficial@gmail.com>
2022-09-06 10:48:38 +0530

    Working on Second View

--- src/Pages/InterestGroups/InterestGroup/InterestGroup.jsx
+++ src/Pages/InterestGroups/InterestGroup/InterestGroup.jsx
@@ -5,6 +5,8 @@ import styles from "./InterestGroup.module.css";
 
 import fvimg from "./assets/fvimg.png";
 
+
+
 const InterestGroup = () => {
   return (
     <>
@@ -20,8 +22,13 @@ const InterestGroup = () => {
                 aliquam animi adipisci neque ex debitis odio laborum blanditiis
                 fugiat eos tempore.
               </p>
+              <p className={styles.officehrs}>
+                <span>Office Hours</span> Every Sunday 7:30 PM at Discord Lobby
+              </p>
               <div className={styles.fv_buttons}>
-                <button className={styles.create}>Create Learning Circles</button>
+                <button className={styles.create}>
+                  Create Learning Circles
+                </button>
               </div>
             </div>
 
@@ -31,6 +38,22 @@ const InterestGroup = () => {
                 alt="Group Learning Image"
                 className={styles.fv_img}
               />
+            </div>
+          </div>
+        </div>
+
+        <div className={styles.second_view_container}>
+          <div className={styles.second_view}>
+            <div className={styles.sv_texts}>
+              <p className={styles.sv_heading}>Basic Enablement Tasks</p>
+              <p className={styles.sv_contents}>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste,
+                necessitatibus.
+              </p>
+            </div>
+
+            <div className={styles.sv_tasks_container}>
+
             </div>
           </div>
         </div>
