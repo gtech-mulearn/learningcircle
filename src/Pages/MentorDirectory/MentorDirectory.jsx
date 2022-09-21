a5376b88e1c2bd82e681b0ccc958b1199648b38a
Aswin Asok <aswinasokofficial@gmail.com>
2022-09-21 15:02:51 +0530

    Working on the First View

--- src/Pages/MentorDirectory/MentorDirectory.jsx
+++ src/Pages/MentorDirectory/MentorDirectory.jsx
@@ -1,7 +1,9 @@
 import React from "react";
 import Footer from "../../Components/Footer/Footer";
 import Navbar from "../../Components/Navbar/Navbar";
-import styles from "./MentorDirectory";
+import styles from "./MentorDirectory.module.css";
+
+import fvimg from "./assets/fvimg.png";
 
 const MentorDirectory = () => {
   return (
@@ -12,8 +14,28 @@ const MentorDirectory = () => {
           <div className={styles.first_view}>
             <div className={styles.fv_texts}>
               <p className={styles.fv_heading}>
-              µLearn Mentor Direcotory.
+                µLearn <span> Mentor Directory.</span>
               </p>
+              <p className={styles.fv_content}>
+                Welcome to the Menotr Directory of Mulearn. Here you will be
+                able to find a mentor who has already gone down the road through
+                which you are about to embrak a journey on. Happy Learning!.
+              </p>
+              <br />
+              <div className={styles.quote}>
+                <p className={styles.aquote}>
+                  <span>"</span>a piece of knowledge, unlike a piece of physical
+                  property, can be shared by large groups of people without
+                  making anybody poorer.<span>"</span>
+                </p>
+                <p className={styles.quoteby}>
+                  <span className={styles.orange}>― Aaron Chettan</span> , The
+                  Boy Who Could Change the World: The Writings of Aaron Swartz
+                </p>
+              </div>
+            </div>
+            <div className={styles.fv_image}>
+              <img src={fvimg} alt="" className={styles.fv_img} />
             </div>
           </div>
         </div>
