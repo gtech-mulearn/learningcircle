98fcb29e88cf18c3e6459e3e9c5887e40a5b6a2f
Basith P <basithpofficial@gmail.com>
2022-10-24 17:11:25 +0530

    Create Course Card

---
+++ src/Components/CourseCard/CourseCard.jsx
@@ -0,0 +1,17 @@
+import "./CourseCard.css";
+
+const CourseCard = ({ title, desc, duration, link }) => (
+  <div className='container'>
+    <div className='details'>
+      <p className='title'>{title}</p>
+      <p className='desc'>{desc}</p>
+      <p className='duration'>{duration}</p>
+    </div>
+    {/* button */}
+    <a href={link} className='cta'>
+      Checkout Course
+    </a>
+  </div>
+);
+
+export default CourseCard;
