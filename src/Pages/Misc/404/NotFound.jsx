80be01a0287fa88fee40be5f52a55be3cc76b84c
Aswin Asok <aswinasokofficial@gmail.com>
2023-01-02 09:10:58 +0530

    404Page-restructed-folder

--- src/Pages/Misc/404/NotFound.jsx
+++ src/Pages/Misc/404/NotFound.jsx
@@ -1,12 +1,11 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import nfgif from "./assets/notfound/nf.gif";
 import styles from "./NotFound.module.css";
 
 const NotFound = () => {
   return (
     <div className={styles.notfoundpage}>
-      <img src={nfgif} alt="" className={styles.nf_img} />
+      <img src="/assets/notfound/fvimg.gif" alt="" className={styles.nf_img} />
       <h1 className={styles.h1}>404 PAGE NOT FOUND</h1>
       <p className={styles.p}>
         The page you are looking for might have been removed, had its name
