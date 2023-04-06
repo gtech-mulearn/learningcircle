97a39054b38ebf0d05136ee7310be6387fff26ae
Aswin Asok <aswinasokofficial@gmail.com>
2023-04-06 11:27:31 +0530

    challenge-enablement-section

--- src/Pages/Practice/Challenges/MuLanding.jsx
+++ src/Pages/Practice/Challenges/MuLanding.jsx
@@ -34,6 +34,86 @@ const MuLanding = () => {
         <div className={styles.second_view_container}>
           <div className={styles.second_view}>
             <div className={styles.sv_texts}>
+              <p className={styles.sv_heading}>General Enablement Tasks</p>
+              <p className={styles.sv_content}>
+                Let's get started with some general enablement tasks to get you
+                familiar with how the basic tools work and get your inital karma
+                points.
+              </p>
+            </div>
+            <div className={styles.mentor_container}>
+              <div className={styles.mentors}>
+                <MentorCard
+                  name="Self Introduction"
+                  designation="You know yourself the best, yet presenting a good self may allow others to get to know you. Posting an introduction to yourself is an important first step in connecting with the community."
+                  mclink="/common/enablement/1"
+                  source="20 Karma Points"
+                />
+                <MentorCard
+                  name="Introduction to Discord"
+                  designation="Are you brand new to Discord? Not to fear, we've got you covered. This course will walk you through the fundamentals to advanced features of discord."
+                  mclink="/common/enablement/2"
+                  source="50 Karma Points"
+                />
+                <MentorCard
+                  name="Intro to µLearn"
+                  designation="Welcome to the Land of μLearn. Time to start your µ-Journey! "
+                  mclink="/challenge/intro-to-mulearn"
+                  source="50 Karma Points"
+                />
+                <MentorCard
+                  name="Introduction to GitHub"
+                  designation="The aim of the course is to teach you about GitHub and its numerous tools and processes, such as Issues and Pull Requests."
+                  mclink="/challenge/intro-to-github"
+                  source="200 Karma Points"
+                />
+                <MentorCard
+                  name="Introduction to Markdown"
+                  designation="The aim of the course is to learn how to communicate using Markdown, a lightweight language for text formatting."
+                  mclink="/challenge/intro-to-markdown"
+                  source="200 Karma Points"
+                />
+                <MentorCard
+                  name="Introduction to Command Line"
+                  designation="Complete the course end to end, Go through all commands and try it in your own terminal"
+                  mclink="/challenge/intro-to-command-line"
+                  source="200 Karma Points"
+                />
+                <MentorCard
+                  name="Introduction to GitHub Pages"
+                  designation="In this course, you will learn to create a site or blog from your GitHub repositories with GitHub Pages."
+                  mclink="/challenge/intro-to-github-pages"
+                  source="200 Karma Points"
+                />
+                <MentorCard
+                  name="Introduction to HTML"
+                  designation="In this course, you'll learn the most common HTML tags by building your own cat photo app."
+                  mclink="/challenge/intro-to-html"
+                  source="100 Karma Points"
+                />
+
+                <MentorCard
+                  name="µLearn Typing Challenge"
+                  designation="Learning to master the skill of typing is always an advantage. It enables a person to be much more productive and use their time efficiently and also increase the outlook of others on them."
+                  mclink="/challenge/typing"
+                  source="800 Karma Points"
+                />
+                <MentorCard
+                  name="TATA Strive Defensive Driving"
+                  designation="This course in this challenge is designed to educate you about
+                  defensive driving & how you can use these tips and strategies to
+                  create a safer place for you as drivers and all those who use
+                  the roads."
+                  mclink="/challenge/defensivedriving"
+                  source="400 Karma Points"
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className={styles.second_view_container}>
+          <div className={styles.second_view}>
+            <div className={styles.sv_texts}>
               <p className={styles.sv_heading}>Existing Challenges</p>
               <p className={styles.sv_content}>
                 Sounds Interesting? Come lets dive more deep into the challenge
@@ -42,6 +122,11 @@ const MuLanding = () => {
             </div>
             <div className={styles.mentor_container}>
               <div className={styles.mentors}>
+                <MentorCard
+                  name="Bring Every Business Online"
+                  designation="Awesome aka Bring Every Business Online is an Open Source Initiative to make an open source collection of web-pages for every business out there. To contribute to the project, all you have to do is build a website for a store or business."
+                  mclink="https://awesome.mulearn.org"
+                />
                 <MentorCard
                   name="Engineering Design Challenge"
                   designation="This challenge challenges engineering students to identify
@@ -66,64 +151,6 @@ const MuLanding = () => {
                   Nations 17 Sustainable Development Goals using Google
                   Technology."
                   mclink="/challenge/googlesolution"
-                />
-                <MentorCard
-                  name="Self Introduction"
-                  designation="You know yourself the best, yet presenting a good self may allow others to get to know you. Posting an introduction to yourself is an important first step in connecting with the community."
-                  mclink="/common/enablement/1"
-                />
-                <MentorCard
-                  name="Introduction to Discord"
-                  designation="Are you brand new to Discord? Not to fear, we've got you covered. This course will walk you through the fundamentals to advanced features of discord."
-                  mclink="/common/enablement/2"
-                />
-                <MentorCard
-                  name="Intro to µLearn"
-                  designation="Welcome to the Land of μLearn. Time to start your µ-Journey! "
-                  mclink="/challenge/intro-to-mulearn"
-                />
-                <MentorCard
-                  name="Introduction to GitHub"
-                  designation="The aim of the course is to teach you about GitHub and its numerous tools and processes, such as Issues and Pull Requests."
-                  mclink="/challenge/intro-to-github"
-                />
-                <MentorCard
-                  name="Introduction to Markdown"
-                  designation="The aim of the course is to learn how to communicate using Markdown, a lightweight language for text formatting."
-                  mclink="/challenge/intro-to-markdown"
-                />
-                <MentorCard
-                  name="Introduction to Command Line"
-                  designation="Complete the course end to end, Go through all commands and try it in your own terminal"
-                  mclink="/challenge/intro-to-command-line"
-                />
-                <MentorCard
-                  name="Introduction to GitHub Pages"
-                  designation="In this course, you will learn to create a site or blog from your GitHub repositories with GitHub Pages."
-                  mclink="/challenge/intro-to-github-pages"
-                />
-                <MentorCard
-                  name="Introduction to HTML"
-                  designation="In this course, you'll learn the most common HTML tags by building your own cat photo app."
-                  mclink="/challenge/intro-to-html"
-                />
-                <MentorCard
-                  name="Bring Every Business Online"
-                  designation="Awesome aka Bring Every Business Online is an Open Source Initiative to make an open source collection of web-pages for every business out there. To contribute to the project, all you have to do is build a website for a store or business."
-                  mclink="https://awesome.mulearn.org"
-                />
-                <MentorCard
-                  name="µLearn Typing Challenge"
-                  designation="Learning to master the skill of typing is always an advantage. It enables a person to be much more productive and use their time efficiently and also increase the outlook of others on them."
-                  mclink="/challenge/typing"
-                />
-                <MentorCard
-                  name="TATA Strive Defensive Driving"
-                  designation="This course in this challenge is designed to educate you about
-                  defensive driving & how you can use these tips and strategies to
-                  create a safer place for you as drivers and all those who use
-                  the roads."
-                  mclink="/challenge/defensivedriving"
                 />
                 <MentorCard
                   name="IoT 101"
