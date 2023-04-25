d9000ae436e498d364f185bef199d2abe2c82792
Bijo Mathew Jose <bijomathewjose@gmail.com>
2023-04-25 12:54:33 +0530

    refactors:android-compose

--- src/Pages/Practice/Challenges/AndroidCompose/AndroidCompose.jsx
+++ src/Pages/Practice/Challenges/AndroidCompose/AndroidCompose.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import styles from "./AndroidCompose.module.css";
 import Navbar from "../../../../Components/Navbar/Navbar";
 import Footer from "../../../../Components/Footer/Footer";
-
 const AndroidCompose = () => {
   return (
     <>
@@ -58,9 +57,9 @@ const AndroidCompose = () => {
                     <div className="timeline__event__title">Step One</div>
                     <div className="timeline__event__description">
                       Learn programming basics and create your first Android app
-                      and share your badges in the <b>&nbsp;#android-development</b>
-                      &nbsp; channel along with the hashtag{" "}
-                      <b>&nbsp;#cl-android-unit1</b>&nbsp; Completing this unit will
+                      and share your Google Developer Profile in the<b>&nbsp;#android-development</b>
+                      &nbsp;channel along with the hashtag
+                      <b>&nbsp;#cl-android-unit1</b>.&nbsp; Completing this unit will
                       earn you&nbsp;
                       <b>100 karma points.</b>
                     </div>
@@ -81,8 +80,7 @@ const AndroidCompose = () => {
                       Continue learning the fundamentals of Kotlin and start
                       building more interactive apps. Share your progress in the
                       <b>&nbsp;#android-development</b> channel along with the hashtag
-                      <b>&nbsp;#cl-android-unit2</b> and don't forget to share your
-                      badges. Completing this unit will earn you&nbsp;
+                      <b>&nbsp;#cl-android-unit2</b> and share your Google Developer Profile. Completing this unit will earn you&nbsp;
                       <b>100 karma points.</b>
                     </div>
                   </div>
@@ -101,9 +99,9 @@ const AndroidCompose = () => {
                     <div className="timeline__event__description">
                       Build apps that display a list of data and make them more
                       beautiful with Material Design. Share your creations in
-                      the <b>&nbsp;#android-development</b> channel along with the
+                      the<b>&nbsp;#android-development</b> channel along with the
                       hashtag
-                      <b>&nbsp;#cl-android-unit3</b> and share your badges. Completing
+                      <b>&nbsp;#cl-android-unit3</b> and share your Google Developer Profile. Completing
                       this unit will earn you <b>100 karma points.</b>
                     </div>
                   </div>
@@ -123,9 +121,8 @@ const AndroidCompose = () => {
                       Learn the best practices of app architecture to build more
                       complex apps. Share your insights and experiences in the
                       <b>&nbsp;#android-development</b> channel along with the hashtag
-                      <b>&nbsp;#cl-android-unit4</b> and share your badges when you
-                      complete the unit. Completing this unit will earn
-                      you&nbsp;
+                      <b>&nbsp;#cl-android-unit4</b> and share your Google Developer Profile when you
+                      complete the unit. Completing this unit will earn you&nbsp;
                       <b>100 karma points.</b>
                     </div>
                   </div>
@@ -145,8 +142,8 @@ const AndroidCompose = () => {
                       Use Kotlin coroutines and Retrofit to get data from the
                       internet and display images in your app using the Coil
                       library. Share your progress and any challenges you faced
-                      in the <b>&nbsp;#android-development</b> channel along with the
-                      hashtag <b>&nbsp;#cl-android-unit5</b> and don't forget to share your
+                      in the<b>&nbsp;#android-development</b> channel along with the
+                      hashtag<b>&nbsp;#cl-android-unit5</b> and share your Google Developer Profile to share your
                       badges. Completing this unit will earn you&nbsp;
                       <b>100 karma points.</b>
                     </div>
@@ -166,9 +163,9 @@ const AndroidCompose = () => {
                     <div className="timeline__event__description">
                       Learn how to store data locally on the device for a smooth
                       and consistent user experience. Share your learnings and
-                      any tips you have in the <b>&nbsp;#android-development</b>&nbsp;
-                      channel along with the hashtag <b>&nbsp;#cl-android-unit6</b>
-                      &nbsp; and share your badges when you complete the unit.
+                      any tips you have in the<b>&nbsp;#android-development</b>&nbsp;
+                      channel along with the hashtag<b>&nbsp;#cl-android-unit6</b>
+                      &nbsp;and share your Google Developer Profile when you complete the unit.
                       Completing this unit will earn you&nbsp;
                       <b>100 karma points.</b>
                     </div>
@@ -189,9 +186,8 @@ const AndroidCompose = () => {
                       Use Android Jetpack’s WorkManager API to schedule
                       necessary background work. Share your experience in the
                       <b>&nbsp;#android-development</b> channel along with the hashtag
-                      <b>&nbsp;#cl-android-unit7</b> and share your badges when you
-                      complete the unit. Completing this unit will earn
-                      you&nbsp;
+                      <b>&nbsp;#cl-android-unit7</b> and share your Google Developer Profile when you
+                      complete the unit. Completing this unit will earn you&nbsp;
                       <b>100 karma points.</b>
                     </div>
                   </div>
@@ -212,7 +208,7 @@ const AndroidCompose = () => {
                       and any challenges you faced in the
                       <b>&nbsp;#android-development</b> channel along with the hashtag
                       <b>&nbsp;#cl-android-unit8</b> and
-                      don't forget to share your badges. Completing this unit
+                      share your Google Developer Profile. Completing this unit
                       will earn you <b>100 karma points.</b>
                     </div>
                   </div>
@@ -222,19 +218,18 @@ const AndroidCompose = () => {
             <div className={styles.sample_container}>
               <p className={styles.sample_header}>Example Message</p>
               <p className={styles.sample_message}>
-                <b>&nbsp;#android-development</b>
+                <b>&nbsp;#cl-android-unit1</b>
                 <br />
-                Hey, I have completed the eighth unit the Android track. Here
-                are my badges for the same.
+                Hey, I have completed the 1st unit of the Android Basics with Compose track.
+                <br /> Here's my Google Developer Profile :&nbsp;<a href="https://g.dev/vinubalagopalap">https://g.dev/vinubalagopalap</a>
               </p>
+              <img src="/assets/challenge/google-developer.png" alt="google developer profile" />
             </div>
           </div>
         </div>
       </div>
-
       <Footer />
     </>
   );
 };
-
 export default AndroidCompose;
